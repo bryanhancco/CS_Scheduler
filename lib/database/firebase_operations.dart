7e19f40be8f432cde382a1ebf841e336ba9b652a
bryanhancco <bhanccoco@unsa.edu.pe>
2023-11-22 12:04:53 -0500

    Agregando la funcion de eliminar de la base de datos

--- lib/database/firebase_operations.dart
+++ lib/database/firebase_operations.dart
@@ -1,13 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:scheduler/classes/models.dart';
 
 const String db = 'cursos';
 
+// Funcion para agregar nuevo curso en Firestore 
 Future createCourse({required Curso curso}) async {
   final course = FirebaseFirestore.instance.collection(db);
   await course.doc(curso.CurCod).set(curso.toMap());
 }
 
+// Funcion para leer todos los cursos de Firestore 
 Future<List> readCourses() async {
   List cursos = [];
   CollectionReference collectionReferenceCursos =
@@ -20,8 +23,13 @@ Future<List> readCourses() async {
   return cursos;
 }
 
-Future<void> updateCourse(
+// Funcion para actualizar un curso en específico de Firestore
+Future<void> updateCourse (
     {required String courseId, required Curso curso}) async {
   final course = FirebaseFirestore.instance.collection(db);
   await course.doc(courseId).update(curso.toMap());
+}
+
+Future<void> deleteCourse(String curCod) async {
+  await FirebaseFirestore.instance.collection(db).doc(curCod).delete();
 }
