89c3bfc52f44376ddaa728b7a20e1b791acd896e
stevenAnto <ecalcinap@unsa.edu.pe>
2023-11-24 22:34:19 -0500

    se arreglo bug de cuando no ingresa codigo app se congela, mostrando mensaje que almenos debe llenar un codiog

--- lib/utilities/create_course_dialog_box.dart
+++ lib/utilities/create_course_dialog_box.dart
@@ -7,16 +7,16 @@ class CreateCourseDialogBox extends StatefulWidget {
   final controllerCourseName;
   final VoidCallback onSave;
   final VoidCallback onCancel;
-  final esObligatorio; 
+  final esObligatorio;
 
-  const CreateCourseDialogBox(
-      {Key? key,
-      required this.controllerCourseShortName,
-      required this.controllerCourseName,
-      required this.onSave,
-      required this.onCancel,
-      required this.esObligatorio,
-      }) : super(key: key);
+  const CreateCourseDialogBox({
+    Key? key,
+    required this.controllerCourseShortName,
+    required this.controllerCourseName,
+    required this.onSave,
+    required this.onCancel,
+    required this.esObligatorio,
+  }) : super(key: key);
   @override
   _CreateCourseDialogBoxState createState() => _CreateCourseDialogBoxState();
 }
@@ -24,115 +24,144 @@ class CreateCourseDialogBox extends StatefulWidget {
 class _CreateCourseDialogBoxState extends State<CreateCourseDialogBox> {
   // This widget is the root of your application.
   bool selectedOption = false;
+  bool isShortNameValid = false;
+  String botonHabilitado = " ";
+  @override
+  void initState() {
+    super.initState();
+
+    // Agrega un listener al controlador del campo shortName
+    widget.controllerCourseShortName.addListener(() {
+      setState(() {
+        isShortNameValid = widget.controllerCourseShortName.text.isNotEmpty;
+      });
+    });
+  }
+
+  void mostrarTexto() {
+    setState(() {
+      if (isShortNameValid) {
+        botonHabilitado = "";
+      } else {
+        botonHabilitado = "Ingrese un codigo";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     MediaQueryData queryData = MediaQuery.of(context);
-    return AlertDialog(
-      shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(15))
-          ),      
-      content: Container(
-        width: queryData.size.width*0.6,
-        height: 390,
-
-        child: Column(
-          children: [
-            Container(
-              
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: 
-                  [    
-                    const Text(
-                      "Creando Curso...",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-    
-                        color: Colors.black,  
+    return SingleChildScrollView(
+      child: AlertDialog(
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(15))),
+        content: Container(
+            width: queryData.size.width * 0.6,
+            child: Column(children: [
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        "Creando Curso...",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
                         ),
-                    ),
-                    IconButton(
-                      onPressed: widget.onCancel,
-                      icon: const Icon(
-                        Icons.cancel,
-                        color: Colors.black,)
-                    ),
-                  ],
+                      ),
+                      IconButton(
+                          onPressed: widget.onCancel,
+                          icon: const Icon(
+                            Icons.cancel,
+                            color: Colors.black,
+                          )),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            Container(
-              height: 330,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text('Nombre del Curso:'),
-                  TextField(
-                    controller: widget.controllerCourseName,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "Sistemas Operat...",
+              Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text('Nombre del Curso:'),
+                    TextField(
+                      controller: widget.controllerCourseName,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Sistemas Operat...",
+                      ),
                     ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text("Nombre Corto:"),
-                      Container(
-                        width: 100,
-                        child: TextField(
-                          controller: widget.controllerCourseShortName,
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(),
-                            hintText: "SO",
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text("Nombre Corto:"),
+                        Container(
+                          width: 100,
+                          child: TextField(
+                            controller: widget.controllerCourseShortName,
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: "SO",
+                            ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text("¿Es obligatorio?"),
-                      Checkbox(
-                        value: selectedOption, 
-                        onChanged:(bool? value) {
-                          setState(() {
-                            selectedOption = !selectedOption;
-                            widget.esObligatorio.text = "${selectedOption}";
-                          });
-                        },
-                        side: MaterialStateBorderSide.resolveWith(
-                            (states) => const BorderSide(width: 1.0, color: Colors.black),
-                        ),
-                        fillColor: MaterialStateProperty.resolveWith<Color>((Set<MaterialState> states) {
-                          return Colors.white;
-                        }),
-                        checkColor: Colors.black,
-                      ),
-                    ],
-                  ),
-                  Center(
-                    child: MaterialButton(
-                      
-                      height: 45,
-                      onPressed: widget.onSave,
-                      color: const Color.fromRGBO(0, 137, 236, 1),
-                      child: const Text(
-                        "Agregar",
-                        style: TextStyle(color: Colors.white),),
+                      ],
                     ),
-                  ),
-                ].map((widget) => Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: widget,
-                )).toList(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text("¿Es obligatorio?"),
+                        Checkbox(
+                          value: selectedOption,
+                          onChanged: (bool? value) {
+                            setState(() {
+                              selectedOption = !selectedOption;
+                              widget.esObligatorio.text = "${selectedOption}";
+                            });
+                          },
+                          side: MaterialStateBorderSide.resolveWith(
+                            (states) => const BorderSide(
+                                width: 1.0, color: Colors.black),
+                          ),
+                          fillColor: MaterialStateProperty.resolveWith<Color>(
+                              (Set<MaterialState> states) {
+                            return Colors.white;
+                          }),
+                          checkColor: Colors.black,
+                        ),
+                      ],
+                    ),
+                    Center(
+                      child: MaterialButton(
+                        height: 45,
+                        onPressed: () {
+                          if (isShortNameValid) {
+                            widget.onSave();
+                          } else {
+                            mostrarTexto();
+                          }
+                        },
+                        color: isShortNameValid
+                            ? const Color.fromRGBO(0, 137, 236, 1)
+                            : const Color.fromRGBO(0, 137, 236, 0.5),
+                        child: const Text(
+                          "Agregar",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    Center(child: Text(botonHabilitado))
+                  ]
+                      .map((widget) => Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: widget,
+                          ))
+                      .toList(),
+                ),
               ),
-            ),
-          ]
-        )
+            ])),
       ),
     );
   }
