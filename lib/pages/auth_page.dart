85fe8349ef94ca95935e805e55fb061846daf26b
stevenAnto <ecalcinap@unsa.edu.pe>
2023-11-28 06:47:00 -0500

    se elimino cuadro de dialogo de confirmacion de eliminacion de curso

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -10,15 +10,15 @@ class AuthPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return const Home();
-          } else {
-            return const LoginScreen();
-          }
-        }
-      ),
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return const Home();
+            } else {
+              /// return const LoginScreen();
+              return const Home();
+            }
+          }),
     );
   }
 }
